6bd59f082981eb218b80a939502aa7caa34f081b
Your name <jazz2001@126.com>
2019-08-07 14:03:42 +0800

    p

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
-require 'minitest/reporters'
-Minitest::reporters.use!
+
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
