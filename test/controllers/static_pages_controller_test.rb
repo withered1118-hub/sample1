02a96f73cbcb443859dc1cfd9e301eb08361fee6
Your name <jazz2001@126.com>
2019-08-07 10:55:47 +0800

    p

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -24,4 +24,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
+
+  
 end
