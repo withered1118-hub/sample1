69e00c73f2e97772e48431311edd1ee790207869
Your name <jazz2001@126.com>
2019-09-02 11:08:01 +0800

    p

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,8 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+//= reqiure jquery
+//= require bootstrap
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
--- test/integration/user_signup_test.rb
+++ test/integration/user_signup_test.rb
@@ -22,5 +22,6 @@ class UserSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
+    assert is_logged_in?
   end
 end
